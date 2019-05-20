b627d8ea657202a46784fd031d173c3608e4281d
Denali Balser <denali.balser167@gmail.com>
2019-05-20 19:17:49 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def instantialize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name= (new_name)
+    @name = new_name
+  end
+  def instantialize(job)
+    @job = job
+  end
+  def job
+    @job
+  end
+  def job= (new_job)
+    @job = new_job
+  end
+end
